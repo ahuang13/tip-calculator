f887f4caa1e42af3212477dd709fd66a51a2096c
Angus Huang <angus@edmodo.com>
2013-12-03 22:04:55 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Angus Huang on 12/3/13.
+//  Copyright (c) 2013 Angus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
