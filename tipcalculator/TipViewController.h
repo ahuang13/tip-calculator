451725e1580c81474b5085ea83093e7b4262eefc
Angus Huang <angus@edmodo.com>
2013-12-14 09:15:13 -0800

    Set up initial tip calculator from video walkthrough.

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Angus Huang on 12/3/13.
+//  Copyright (c) 2013 Angus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
